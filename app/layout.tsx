65eaa99eac01bf882c6d668ae29619422296fb8b
Aaron <java.jaydev@gmail.com>
2023-06-05 16:32:04 +0900

    Auth UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,30 @@
+import ClientOnly from './components/ClientOnly';
+import RegisterModal from './components/modals/RegisterModal';
 import Navbar from './components/navbar/Navbar';
 import './globals.css';
 import { Nunito } from 'next/font/google';
+import ToasterProvider from './providers/ToasterProvider';
 
 export const metadata = {
-    title: 'Airbnb',
-    description: 'Airbnb Clone Coding',
+  title: 'Airbnb',
+  description: 'Airbnb Clone Coding',
 };
 
 const font = Nunito({
-    subsets: ['latin'],
+  subsets: ['latin'],
 });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <html lang='en'>
-            <body className={font.className}>
-                <Navbar />
-                {children}
-            </body>
-        </html>
-    );
+  return (
+    <html lang='en'>
+      <body className={font.className}>
+        <ClientOnly>
+          <ToasterProvider />
+          <RegisterModal />
+          <Navbar />
+        </ClientOnly>
+        {children}
+      </body>
+    </html>
+  );
 }
