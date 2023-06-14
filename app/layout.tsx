c7f36f6f4950a29b42fe7bff3be82d29aad128cf
Aaron <java.jaydev@gmail.com>
2023-06-14 16:44:18 +0900

    Listing creation step 1 (Category selection)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
 import { Nunito } from 'next/font/google';
 
 import './globals.css';
+
 import Navbar from './components/navbar/Navbar';
 import ClientOnly from './components/ClientOnly';
 import RegisterModal from './components/modals/RegisterModal';
 import LoginModal from './components/modals/LoginModal';
+import RentModal from './components/modals/RentModal';
+
 import ToasterProvider from './providers/ToasterProvider';
 import getCurrentUser from './actions/getCurrentUser';
 
@@ -25,8 +28,9 @@ export default async function RootLayout({ children }: { children: React.ReactNo
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
-          <RegisterModal />
+          <RentModal />
           <LoginModal />
+          <RegisterModal />
           <Navbar currentUser={currentUser} />
         </ClientOnly>
         {children}
